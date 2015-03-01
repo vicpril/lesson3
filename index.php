f5e7e33cf2002aedcfec8790dd453a3a9215aca0
Viktor Prilepin <vicpride@gmail.com>
2015-03-01 22:34:35 +0600

    work on bugs - 1

--- index.php
+++ index.php
@@ -1,17 +1,66 @@
 <?php
+
 header('Content-type: text/html; charset=utf-8');
 error_reporting(E_ALL);
 // Lesson 3
 
+//echo date('d.m.Y H:i:s', mktime(0,0,0,0,0,0))."\n";       //print zero of timestemp
+
 $date = array();
 mt_srand(time());
-for ($i = 0; $i <= 4; $i++){                //fill $date by 5 element of random timestamp
+for ($i = 0; $i <= 4; $i++) {              //fill $date by 5 element of random timestamp
     $date[$i] = mt_rand(0, time());
 }
-//echo var_dump($date);
 
-echo "<br>Min date: ".date('D, d.m.Y H:m:s', min($date));   //print min & max days
-echo "<br>Max date: ".date('D, d.m.Y H:m:s', max($date));
+////SOLUTION #1
+//foreach ($date as $value) {                //print $date in data-format
+//    echo date('D, d.m.Y H:i:s', $value) . "\n";
+//}
+//$days = array();                            
+//
+//foreach ($date as $value) {                 //parse $date by day in $days
+//    $days[] = (int) date('d', $value);
+//}
+//echo "<br>Min day: ".min($days)."\n";       //print min of $days
+//
+//$months = array(); 
+//foreach ($date as $value) {                 //parse $date by month in $month
+//    $months[] = (int) date('m', $value);
+//}
+//$month = max($months);
+//echo "<br>Max month: ".$month.'='. date('F', mktime(0,0,0,$month+1,0,0))."\n";          //print max of $month
+
+//SOLUTION #2
+
+foreach ($date as $value) {                         //print $date in data-format
+    echo date('D, d.m.Y H:i:s', $value) . "\n";
+}
+echo"____________________\n";
+
+function sortByDay($a,$b)                           
+{
+    if ((int)date('d',$a) == (int)date('d',$b)){
+        return 0;
+    }
+    return ((int)date('d',$a) < (int)date('d',$b)) ? -1 : 1;
+}
+function sortByMonth($a,$b)
+{
+    if ((int)date('m',$a) == (int)date('m',$b)){
+        return 0;
+    }
+    return ((int)date('m',$a) < (int)date('m',$b)) ? -1 : 1;
+}
+
+usort($date, "sortByDay");
+echo "<br>Min day: ".(int)date('d',$date[0])."\n";
+
+usort($date, "sortByMonth");
+echo "<br>Max month: ".(int)date('m',$date[count($date)-1])."=".date('F',$date[count($date)-1])."\n";
+
+//foreach ($date as $value) {                //print $date in data-format
+//    echo date('D, d.m.Y H:i:s', $value) . "\n";
+//}
 
 sort($date);                                //sort $date from lowest to highest
 //echo var_dump($date);
@@ -20,8 +69,10 @@ $selected = array_pop($date);               //cut last element $date to $selecte
 //echo var_dump($date);
 //echo var_dump($selected);
 
-echo "<br>\$selected = ".date('d.m.Y H:m:s', $selected);    //print $selected in data-format
+echo "<br>\$selected = " . date('d.m.Y H:i:s', $selected)."\n";    //print $selected in data-format
 
 date_default_timezone_set('America/New_York');
-echo "<br>\$selected (in NY) = ".date('d.m.Y H:m:s', $selected);  //print $selected in NY-timezone
+echo "<br>\$selected (in NY) = " . date('d.m.Y H:i:s', $selected)."\n";  //print $selected in NY-timezone
+echo "<br>New Timezone is ".  date_default_timezone_get();
+
 ?>
